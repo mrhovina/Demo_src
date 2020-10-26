de986c62f781f853f3e6ffdff122c3e08607d24d
Luka Frelih <frelih.luka@gmail.com>
2020-10-26 11:44:46 +0100

    Added Website that can view and create users using the DemoAPI

---
+++ BlazorApp1/Models/EmployeeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp1.Models
+{
+    public partial class EmployeeViewModel
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage ="Full name is required")]
+        public string employee_name { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage ="Incorrect value for Salary")]
+        public int? employee_salary { get; set; }
+
+        [Range(1, 200, ErrorMessage = "Incorrect value for Age")]
+        public int? employee_age { get; set; }
+        public string pictureBase64 { get; set; }
+    }
+}
